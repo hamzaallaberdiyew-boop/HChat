789ee360f08263bc510a7af14a66b3bbd162dcbb
Hamza Allaberdiyev <hamzaallaberdiyew@gmail.com>
2026-06-05 21:35:31 +0900

    Connect Supabase database

--- server/db.js
+++ server/db.js
@@ -1,3 +1,20 @@
+// import pg from 'pg';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: String(process.env.DB_PASSWORD),
+// });
+
+export default pool;
+
 import pg from 'pg';
 import dotenv from 'dotenv';
 
@@ -6,11 +23,10 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
