19d3c69c5bec18398389a39d87768e788ea3e609
Hamza Allaberdiyev <hamzaallaberdiyew@gmail.com>
2026-06-06 18:02:57 +0900

    fix db.js duplicate export

--- server/db.js
+++ server/db.js
@@ -1,20 +1,3 @@
-// import pg from 'pg';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const { Pool } = pg;
-
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   database: process.env.DB_NAME,
-//   user: process.env.DB_USER,
-//   password: String(process.env.DB_PASSWORD),
-// });
-
-export default pool;
-
 import pg from 'pg';
 import dotenv from 'dotenv';
 
@@ -23,10 +6,11 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 export default pool;
